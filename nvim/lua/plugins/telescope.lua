0d3620a6f8ab9b03a135d88aa3246e244aebdf92
akashddas <akashddas@zohomail.in>
2025-01-19 19:07:02 +0530

    Add in keymapping to show all the keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,7 +6,6 @@ return {
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 	},
 	config = function()
-		local map = vim.keymap.set
 		require("telescope").setup({
 			pickers = {
 				live_grep = {
@@ -20,11 +19,13 @@ return {
 				fzf = {},
 			},
 		})
+		local map = vim.keymap.set
 		local builtin = require("telescope.builtin")
 		map("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		map("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		map("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		map("n", "<leader>fb", builtin.buffers, { desc = "Telescope show buffers" })
 		map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		map("n", "<leader>fk", builtin.keymaps, { desc = "Telescope show keymaps" })
 		map("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope find references" })
 		map("n", "<leader>vn", function()
 			builtin.find_files({
