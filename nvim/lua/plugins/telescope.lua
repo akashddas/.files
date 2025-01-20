e2273e2b1924e3879dc7ed8baec3799a6778796c
akashddas <akashddas@zohomail.in>
2025-01-20 10:22:17 +0530

    Make slight changes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -21,21 +21,16 @@ return {
 		})
 		local map = vim.keymap.set
 		local builtin = require("telescope.builtin")
+		map("n", "<leader>bb", builtin.buffers, { desc = "Telescope show buffers" })
+		map("n", "<leader>gs", builtin.live_grep, { desc = "Telescope live grep" })
 		map("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		map("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		map("n", "<leader>fb", builtin.buffers, { desc = "Telescope show buffers" })
 		map("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 		map("n", "<leader>fk", builtin.keymaps, { desc = "Telescope show keymaps" })
 		map("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope find references" })
-		map("n", "<leader>vn", function()
+		map("n", "<leader>dot", function()
 			builtin.find_files({
-				cwd = vim.fn.stdpath("config"),
+				cwd = vim.fn.getenv("DOTFILES"),
 			})
-		end, { desc = "Telescope jump to neovim config directory" })
-		map("n", "<leader>vz", function()
-			builtin.find_files({
-				cwd = "$DOTFILES/zsh/",
-			})
-		end, { desc = "Telescope jump to zsh config directory" })
+		end, { desc = "Telescope open dotfiles directory" })
 	end,
 }
