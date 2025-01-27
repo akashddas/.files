0e89c9622604104066d51b3e1651a4194e28340a
akashddas <akashddas@zohomail.in>
2025-01-27 14:43:02 +0530

    Save

--- rofi/scripts/rofi-wifi-menu.sh
+++ rofi/scripts/rofi-wifi-menu.sh
@@ -13,8 +13,13 @@ YOFF=0
 XOFF=0
 FONT="IntoneMono Nerd Font Medium 14"
 
-LIST=$(nmcli --fields "$FIELDS" device wifi list | sed '/^--/d')
-# For some reason rofi always approximates character width 2 short... hmmm
+LIST=$(
+  nmcli --fields "$FIELDS" device wifi list |
+    sed '/^--/d' |
+    sed '1d' |
+    awk '!seen[$1]++'
+)
+# Change rofi display width
 RWIDTH=$(($(echo "$LIST" | head -n 1 | awk '{print length($0); }') + 2))
 # Dynamically change the height of the rofi menu
 LINENUM=$(echo "$LIST" | wc -l)
@@ -23,10 +28,20 @@ KNOWNCON=$(nmcli connection show)
 # Really janky way of telling if there is currently a connection
 CONSTATE=$(nmcli -fields WIFI g)
 
-CURRSSID=$(LANGUAGE=C nmcli -t -f active,ssid dev wifi | awk -F: '$1 ~ /^yes/ {print $2}')
+CURRSSID=$(
+  LANGUAGE=C nmcli -t -f active,ssid dev wifi |
+    awk -F: '$1 ~ /^yes/ {print $2}'
+)
 
 if [[ ! -z $CURRSSID ]]; then
-  HIGHLINE=$(echo "$(echo "$LIST" | awk -F "[  ]{2,}" '{print $1}' | grep -Fxn -m 1 "$CURRSSID" | awk -F ":" '{print $1}') + 1" | bc)
+  HIGHLINE=$(
+    echo "$(
+      echo "$LIST" |
+        awk -F "[  ]{2,}" '{print $1}' |
+        grep -Fxn -m 1 "$CURRSSID" |
+        awk -F ":" '{print $1}'
+    ) + 1" | bc
+  )
 fi
 
 if [[ "$CONSTATE" =~ "enabled" ]]; then
@@ -35,20 +50,28 @@ elif [[ "$CONSTATE" =~ "disabled" ]]; then
   TOGGLE="󰖩  Enable Wi-Fi"
 fi
 
-CHENTRY=$(echo -e "$TOGGLE\nManual\n$LIST" | uniq -u | rofi -dmenu -p "Wi-Fi SSID" -lines "$LINENUM" -a "$HIGHLINE" -location "$POSITION" -yoffset "$YOFF" -xoffset "$XOFF" -font "$FONT" -width -"$RWIDTH")
-#echo "$CHENTRY"
+CHENTRY=$(
+  echo -e "$TOGGLE\nManual\n$LIST" |
+    uniq -u |
+    rofi -dmenu -p "Wi-Fi SSID" -lines "$LINENUM" -a "$HIGHLINE" -location "$POSITION" -yoffset "$YOFF" -xoffset "$XOFF" -font "$FONT" -width -"$RWIDTH"
+)
+# echo "$CHENTRY"
 CHSSID=$(echo "$CHENTRY" | sed 's/\s\{2,\}/\|/g' | awk -F "|" '{print $1}')
-#echo "$CHSSID"
+# echo "$CHSSID"
 
-# If the user inputs "Manual" as their SSID in the start window, it will bring them to this screen
+# If the user inputs "Manual" as their SSID in the start window,
+# bring them to this screen
 if [ "$CHENTRY" = "Manual" ]; then
   # Manual entry of the SSID and password (if appplicable)
-  MSSID=$(echo "enter the SSID of the network (SSID,password)" | rofi -dmenu -p "Manual Entry" -font "$FONT" -lines 1)
+  MSSID=$(
+    echo "enter the SSID of the network (SSID,password)" |
+      rofi -dmenu -p "Manual Entry" -font "$FONT" -lines 1
+  )
   # Separating the password from the entered string
   MPASS=$(echo "$MSSID" | awk -F "," '{print $2}')
 
-  #echo "$MSSID"
-  #echo "$MPASS"
+  # echo "$MSSID"
+  # echo "$MPASS"
 
   # If the user entered a manual password, then use the password nmcli command
   if [ "$MPASS" = "" ]; then
@@ -67,12 +90,13 @@ elif [ "$CHENTRY" = "󰖪  Disable Wi-Fi" ]; then
 
 else
 
-  # If the connection is already in use, then this will still be able to get the SSID
+  # If the connection is already in use, get the SSID
   if [ "$CHSSID" = "*" ]; then
     CHSSID=$(echo "$CHENTRY" | sed 's/\s\{2,\}/\|/g' | awk -F "|" '{print $3}')
   fi
 
-  # Parses the list of preconfigured connections to see if it already contains the chosen SSID. This speeds up the connection process
+  # Parses the list of preconfigured connections to see if it already
+  # contains the chosen SSID. This speeds up the connection process
   if [[ $(echo "$KNOWNCON" | grep "$CHSSID") = "$CHSSID" ]]; then
     nmcli con up "$CHSSID"
   else
@@ -84,5 +108,10 @@ else
     fi
     nmcli dev wifi con "$CHSSID" password "$WIFIPASS"
   fi
-
+  # Check if the connection was successful and notify the user
+  if [[ $? -eq 0 ]]; then
+    notify-send "Successfully connected to $CHSSID"
+  else
+    notify-send "$CHSSID Failed to connect"
+  fi
 fi
